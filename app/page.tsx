14c8cd6da0cf5b62190d2d71be87ac5dbf7b44c4
Aparajita Tiwari <aparajitatiwari202@gmail.com>
2026-04-12 23:09:11 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default async function HomePage() {
             />
             <span className="text-[16px] text-green-600 md:text-lg font-semibold leading-tight">
               AchieveX - Maitreyi <br className="block sm:hidden" />
-              <br className="block sm:hidden" /br>
+              <br className="block sm:hidden" />
               
               
             </span>
@@ -67,7 +67,7 @@ export default async function HomePage() {
            <h1 className="text-2xl sm:text-5xl md:text-4xl font-bold tracking-tight text-balance">
             Application Portal for IQAC  <br className="block sm:hidden" /> Meritorious Student Award
           </h1>
-          <br className="block sm:hidden" /br>
+          <br className="block sm:hidden" />
             <h2 className="text-xl sm:text-2xl md:text-2xl text-gray-600">
       Track Your Academic Excellence
     </h2>
